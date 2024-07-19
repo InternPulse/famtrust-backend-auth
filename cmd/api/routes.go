4c2694cdf30026f9391da6e06afae1c2b654fbb2
Obi.M <mail@obimadu.pro>
2024-07-19 16:08:04 +0100

    feat: make auth routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -11,18 +11,24 @@ func (app *Config) routes() *gin.Engine {
 	// Make router.
 	mux := gin.New()
 
-	// Use Logger
-	mux.Use(gin.Logger())
-	// Use Recovery
-	mux.Use(gin.Recovery())
-	// Use Cors
-	mux.Use(cors.Default())
+	// Use Middlewares
+	mux.Use(gin.Logger())   //Logger
+	mux.Use(gin.Recovery()) //Recovery
+	mux.Use(cors.Default()) //Cors
 
 	// Make api base
 	api := mux.Group("/api")
 
 	// Swagger docs
 	api.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	// Make API v1
+	v1 := api.Group("/v1")
+
+	// Auth Routes
+	v1.GET("/signup", app.Handlers.Users().Signup)
+	v1.GET("/login", app.Handlers.Users().Login)
+	v1.GET("/validate", app.Handlers.Users().Validate)
 
 	return mux
 
