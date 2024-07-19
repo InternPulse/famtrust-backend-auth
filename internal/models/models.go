8632e3558e440dd6dd7625046ab24115d09d129e
Obi.M <mail@obimadu.pro>
2024-07-19 20:00:22 +0100

    feat: implement models interface

--- internal/models/models.go
+++ internal/models/models.go
@@ -7,14 +7,20 @@ import (
 
 type Models struct {
 	user interfaces.UserModels
+	role interfaces.UserRoles
 }
 
 func (m *Models) User() interfaces.UserModels {
 	return m.user
 }
 
+func (m *Models) Role() interfaces.UserRoles {
+	return m.role
+}
+
 func NewModel(DB *gorm.DB) interfaces.Models {
 	return &Models{
 		user: &UserModels{DB: DB},
+		role: &UserRoles{DB: DB},
 	}
 }
