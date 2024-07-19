56bac3a482d78d32660b14f8556fe45d9e4a8c1a
Obi.M <mail@obimadu.pro>
2024-07-19 16:06:26 +0100

    feat: create skeleton handlers/user-handlers implementation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,17 @@
 package handlers
+
+import "github.com/InternPulse/famtrust-backend-auth/internal/interfaces"
+
+type Handlers struct {
+	users interfaces.UserHandlers
+}
+
+func (h *Handlers) Users() interfaces.UserHandlers {
+	return h.users
+}
+
+func NewHandler(models interfaces.Models) interfaces.Handlers {
+	return &Handlers{
+		users: &UserHandlers{models: models},
+	}
+}
