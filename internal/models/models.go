0bbd36d2384c96994d9206a66f7a11fb73ca4eab
Obi.M <mail@obimadu.pro>
2024-07-19 16:06:51 +0100

    feat: create skeleton models/user-models implementation

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,20 @@
 package models
+
+import (
+	"github.com/InternPulse/famtrust-backend-auth/internal/interfaces"
+	"gorm.io/gorm"
+)
+
+type Models struct {
+	user interfaces.UserModels
+}
+
+func (m *Models) User() interfaces.UserModels {
+	return m.user
+}
+
+func NewModel(DB *gorm.DB) interfaces.Models {
+	return &Models{
+		user: &UserModels{DB: DB},
+	}
+}
