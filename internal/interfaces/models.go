2301d5bbe8122fc92778dfb202341bdf3b1b7068
Obi.M <mail@obimadu.pro>
2024-07-19 16:04:59 +0100

    feat: create skeleton models interface

--- internal/interfaces/models.go
+++ internal/interfaces/models.go
@@ -1,3 +1,38 @@
 package interfaces
 
-type Models interface{}
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Models interface {
+	User() UserModels
+}
+
+type UserModels interface {
+}
+
+type User struct {
+	gorm.Model
+	First_name          string
+	Last_name           string
+	Email               string
+	Password_hash       string
+	Bio                 string
+	Profile_picture_url string
+	Role_id             uint
+	Is_freezed          bool
+	Last_login          time.Time
+}
+
+type Role struct {
+	Name              string
+	CanTopUpFamilyAcc bool
+	CanCreateSubAcc   bool
+	CanDeleteSubAcc   bool
+	CanEditFamilyAcc  bool
+	CanSendtoSubAcc   bool
+	CanSendtoBank     bool
+	gorm.Model
+}
