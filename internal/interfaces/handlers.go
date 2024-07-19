88a3a65449a8d8dc853720ead483e98e88905ee0
Obi.M <mail@obimadu.pro>
2024-07-19 16:04:37 +0100

    feat: create skeleton handlers interface

--- internal/interfaces/handlers.go
+++ internal/interfaces/handlers.go
@@ -1,3 +1,13 @@
 package interfaces
 
-type Handlers interface{}
+import "github.com/gin-gonic/gin"
+
+type Handlers interface {
+	Users() UserHandlers
+}
+
+type UserHandlers interface {
+	Signup(c *gin.Context)
+	Login(c *gin.Context)
+	Validate(c *gin.Context)
+}
