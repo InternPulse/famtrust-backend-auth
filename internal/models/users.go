16c909a4a8c4060f4c93128c247477fd3dcbb34b
Obi.M <mail@obimadu.pro>
2024-07-20 23:40:06 +0100

    feat: implement passwordMatches func

--- internal/models/users.go
+++ internal/models/users.go
@@ -1,7 +1,11 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/InternPulse/famtrust-backend-auth/internal/interfaces"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -16,7 +20,7 @@ func (u *UserModels) CreateUser(user *interfaces.User) error {
 	return nil
 }
 
-func (u *UserModels) GetUserByID(userID uint) (*interfaces.User, error) {
+func (u *UserModels) GetUserByID(userID uuid.UUID) (*interfaces.User, error) {
 	var user interfaces.User
 	if err := u.DB.First(&user, userID).Error; err != nil {
 		return nil, err
@@ -39,9 +43,27 @@ func (u *UserModels) UpdateUser(user *interfaces.User) error {
 	return nil
 }
 
-func (u *UserModels) DeleteUserByID(userID uint) error {
+func (u *UserModels) DeleteUserByID(userID uuid.UUID) error {
 	if err := u.DB.Delete(&interfaces.User{}, userID).Error; err != nil {
 		return err
 	}
 	return nil
+}
+
+// PasswordMatches uses Go's bcrypt package to compare a user supplied password
+// with the hash we have stored for a given user in the database. If the password
+// and hash match, we return true; otherwise, we return false.
+func (u *UserModels) PasswordMatches(user *interfaces.User, plainText string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password_hash), []byte(plainText))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			// invalid password
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
 }
