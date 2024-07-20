bfcdc2ea03d50d924ae5ea499652f4fbf2252209
Obi.M <mail@obimadu.pro>
2024-07-20 02:12:08 +0100

    feat: make more project models

--- internal/interfaces/models.go
+++ internal/interfaces/models.go
@@ -3,6 +3,7 @@ package interfaces
 import (
 	"time"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -26,31 +27,57 @@ type UserRoles interface {
 	DeleteRoleByID(roleID uint) error
 }
 
+// Create uuid model.
+type UUIDModel struct {
+	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+// Create uuid BeforeCreate hook.
+func (u *UUIDModel) BeforeCreate(tx *gorm.DB) (err error) {
+	if u.ID == uuid.Nil {
+		u.ID = uuid.New()
+	}
+	return
+}
+
 type User struct {
-	gorm.Model
-	First_name          string    `gorm:"not null"`
-	Last_name           string    `gorm:"not null"`
-	Email               string    `gorm:"not null;unique"`
-	Password_hash       string    `gorm:"not null"`
-	Bio                 string    `gorm:"not null"`
-	NIN                 uint      `gorm:"not null;unique"`
-	BVN                 uint      `gorm:"not null;unique"`
-	Profile_picture_url string    `gorm:"not null"`
-	RoleID              string    `gorm:"not null"`
-	Is_freezed          bool      `gorm:"not null"`
-	Last_login          time.Time `gorm:"not null"`
-	Role                Role      `gorm:"foreignKey:RoleID;references:Name"`
+	UUIDModel
+	Email         string      `gorm:"not null;unique"`
+	Password_hash string      `gorm:"not null"`
+	RoleID        string      `gorm:"not null"`
+	Is_verified   bool        `gorm:"not null"`
+	Is_freezed    bool        `gorm:"not null"`
+	Last_login    time.Time   `gorm:"not null"`
+	Role          Role        `gorm:"foreignKey:RoleID;references:ID"`
+	UserProfile   UserProfile `gorm:"foreignKey:UserID;references:ID;constraints:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+type UserProfile struct {
+	UUIDModel
+	UserID              uuid.UUID
+	First_name          string `gorm:"not null"`
+	Last_name           string `gorm:"not null"`
+	Bio                 string `gorm:"not null"`
+	NIN                 uint   `gorm:"not null;unique"`
+	BVN                 uint   `gorm:"not null;unique"`
+	Profile_picture_url string `gorm:"not null"`
 }
 
 type Role struct {
-	gorm.Model
-	Name                 string `gorm:"not null;unique"`
-	CanOperateFamilyAcct bool   `gorm:"not null"`
-	CanCreateUser        bool   `gorm:"not null"`
-	CanCreateSubAcc      bool   `gorm:"not null"`
-	CanDeleteSubAcc      bool   `gorm:"not null"`
-	CanEditFamilyAcc     bool   `gorm:"not null"`
-	CanSendtoSubAcc      bool   `gorm:"not null"`
-	CanSendtoBank        bool   `gorm:"not null"`
-	CanFreezeSubAcc      bool   `gorm:"not null"`
+	ID         string `gorm:"primaryKey"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
+	Permission []Permission   `gorm:"many2many:role_permissions;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+type Permission struct {
+	ID        string `gorm:"primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Role      []Role         `gorm:"many2many:role_permissions;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
